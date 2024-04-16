fded9e0a1558b83a0669824cf6f517bf89e4d8e8
Jack Luo <jack145945@gmail.com>
2024-04-15 22:00:15 -0400

    Documentation: added documentation on 2 modules

---
+++ model/eps.py
@@ -0,0 +1,9 @@
+"""
+DOCUMENTATION: eps.py
+
+Input: Array of tickers and their respective EPS and PE ratios
+Output: An array of GOOD stocks and an array of REJECTED stocks
+
+FUNCTION
+
+"""
--- model/input.py
+++ model/input.py
@@ -1,6 +1,28 @@
+"""
+DOCUMENTATION: input.py
+
+Input: An array of ALL tickers we want to test
+Output: An array of stock objects 
+
+STOCK Object:
+    - stock_name
+    - ticker
+    - current_price
+    - PE
+    - EPS
+    - EPS_growth
+
+FUNCTION
+Our goal is to go through an API and get all the data and produce
+
+"""
+
 from dotenv import load_dotenv
 import os
 
+# S0: Input
+
+# S1: API Key and set up
 load_dotenv()  # take environment variables from .env.
 
 api_key = os.getenv('QUICKFS_API_KEY')
@@ -9,3 +31,5 @@ if api_key:
     print("API Key:", api_key)
 else:
     print("Environment variable not found.")
+
+# S2: Get data from API
