86e9ceb9980f3a50a4d1372a2a7d7661f08489b1
Jack Luo <jack145945@gmail.com>
2024-04-15 23:34:08 -0400

    input.py: fixed pandas data frame, then created the stock objects

--- model/input.py
+++ model/input.py
@@ -9,6 +9,7 @@ STOCK Object:
     - stock_name
     - EPS_2023
     - EPS_growth (list of 10 year EPS growth)
+    (other data for the future)
 
 FUNCTION
 Our goal is to go through an API and get all the data and produce
@@ -46,8 +47,10 @@ print("S1: Client Success, current usage:", client.get_usage())
 
 # S2: For stock 1, 5, and 12 based on indice, import the stock object (attributes above) and initialize some data points based on the output using API calls, then print out the stock object for testing (all 3)
 
-# get 10 years eps growth for the test stocks 
-resp = client.get_data_batch(companies=test_stocks, metrics=['eps_diluted_growth'], period="FY-9:FY")
+# EPS Module
+
+# get 10 years eps growth, eps, pe ratio, and price for the test stocks
+resp = client.get_data_batch(companies=test_stocks, metrics=['eps_diluted_growth', 'price_to_earnings'], period="FY-9:FY") # Example Function
 
 # Check the status of the call
 print("S2: client resp:", client.resp) # If it says 207, you have content to use.
@@ -57,6 +60,32 @@ print("S2: client content:", client.resp._content) # check the error
 pd_stocks = json.loads(client.resp._content.decode('utf-8'))['data'] # filter and extract for the companies that you have data
 pd_stocks = pd.DataFrame(pd_stocks) # transform it to a pandas dataframe (if you want it)
 print("S3: organized stocks", pd_stocks)
+
+# S4: create stock objects for the test stocks
+# using numpy, get the average EPS growth for each stock, and create the stock object for each stock
+# get the most recent eps growth based on teh last eps diluted growth for each stock for inputting into the stock object later\
+
+# please ignore stock name, and also current prices for now (set name to TEST, and set current price to -1)
+
+# create a list of stock objects
+stock_objects = []
+
+for i in range(len(test_stocks)):
+    stock_name = "TEST"
+    ticker = test_stocks[i]
+    EPS_2023 = 0
+    EPS_growth = pd_stocks.loc[ticker, 'eps_diluted_growth']
+    PE = pd_stocks.loc[ticker, 'price_to_earnings']
+    current_price = -1
+
+    stock = Stock(stock_name, ticker, EPS_2023, EPS_growth, PE, current_price)
+    stock_objects.append(stock)
+
+
+# print the stock objects
+print("S4: Stock Objects")
+for stock in stock_objects:
+    print(stock)
 
 # ARCHIVES
 # Stock 1: AAPL
