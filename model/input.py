b62421e7fdbdd5f82e2ffa5c416cdef257b2878e
Jack Luo <jack145945@gmail.com>
2024-04-15 22:36:56 -0400

    Input.py: Working API and Usage Calls

--- model/input.py
+++ model/input.py
@@ -17,9 +17,15 @@ Our goal is to go through an API and get all the data and produce
 """
 
 from dotenv import load_dotenv
+from quickfs import QuickFS
+from all_stocks import all_stocks
 import os
 
-# S0: Input
+# S0: Input (US indexes Russel 1000 + 2000 V2) (S&P 500 V1)
+
+# print the first 3 stock in the all stock list as testing purposes
+print("INPUT TEST REPORT")
+print("S0:", all_stocks[0], all_stocks[1], all_stocks[2])
 
 # S1: API Key and set up
 load_dotenv()  # take environment variables from .env.
@@ -27,8 +33,14 @@ load_dotenv()  # take environment variables from .env.
 api_key = os.getenv('QUICKFS_API_KEY')
 
 if api_key:
-    print("API Key:", api_key)
+    print("S1: API Key Success")
 else:
-    print("Environment variable not found.")
+    print("S1: Environment variable not found.")
+
+client = QuickFS(api_key)
+print("S1: Client Success, current usage:", client.get_usage())
 
 # S2: Get data from API
+
+
+
