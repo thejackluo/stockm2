5495133604b19e3222cc7187ad862750b3f4cbaa
Jack Luo <jack145945@gmail.com>
2024-05-27 00:42:19 -0400

    v0.1.3: backtesting template

---
+++ backtesting/backtesting.py
@@ -0,0 +1,5 @@
+# calculate predicted buy and sell price
+# determine the expected return and risk from the trade
+
+# given real world trading data, determine signals (buy, sell, etc)
+# determine the performance of the model
