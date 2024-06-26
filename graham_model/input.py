198fc45cd16ca7dbfecd5c586e0826a12df3e7c1
Kiebb <112326161+Kiebb@users.noreply.github.com>
2024-06-26 14:08:22 +0800

    graham model from a book

---
+++ graham_model/input.py
@@ -0,0 +1,124 @@
+"""
+DOCUMENTATION: input.py
+
+INPUT: An array of ALL tickers we want to test
+OUTPUT: An array of stock objects 
+
+STOCK OBJ:
+    - ticker
+    - stock_name
+    - EPS_2023
+    - EPS_growth (list of 10 year EPS growth)
+    (other data for the future)
+
+FUNCTION
+Our goal is to go through an API and get all the data and produce ______________
+
+This file uses QUICKFS as the API of choice
+
+LIMITATIONS:
+Period end price data is only available to the period end price (end of 2023), can be optimized to end of quarter price, but still not ideal
+
+"""
+
+from dotenv import load_dotenv
+from quickfs import QuickFS
+from util.all_stocks import all_stocks
+from obj.Stock import Stock
+import os
+import json
+import pandas as pd
+#from alphavantagemethods import alpha_vantage_current_price_obtainer
+
+# S0: Get input (US indexes Russel 1000 + 2000 V2) (S&P 500 V1)
+"""
+# create a list of the three test stocks (1, 5, 12) ticker only no object
+# test_stocks = [all_stocks[1], all_stocks[5], all_stocks[12]] # print the first 3 stock in the all stock list as testing purposes # AAPL, GOOGL, TSLA????
+"""
+
+print("=====================================")
+print("P0: Input Module")
+print("=====================================") 
+
+test_stocks = all_stocks[:] # test stock list based on all_stocks.py
+print("S0: Stock List:", test_stocks) 
+
+
+# S1: Add API KEY and Set Up QuickFS SDK
+load_dotenv(override=True)  # take environment variables from .env.
+api_key = os.getenv('QUICKFS_API_KEY')
+
+if api_key:
+    print("S1: API Key Success:", api_key)
+    client = QuickFS(api_key)
+    print("S1: QuickFS SDK Client Success, current usage:", client.get_usage())
+else:
+    print("ERROR S1: Environment or API_Key variable not found.")
+
+
+
+# S2: For test_stocks list, RETRIEVE different attributes of the stock based on the API and initialize new stock object
+# EPS Module Data V1
+resp = client.get_data_batch(companies=test_stocks, metrics=['total_current_assets', 'total_current_liabilities','period_end_price','market_cap','shares_diluted'], period="FY-9:FY") # get 10 years eps growth, eps, pe ratio, and price for the test stocks
+# Check the status of the call
+print("S2: client resp number:", client.resp) # Outputs response number. If it says 207, you have content to use.
+print("S2: client content:", client.resp._content) # Outputs the content of the response, if there is an error, check the error
+
+
+# # # EPS Module Data V2: use the client.get_data_full to get metadata for stock and use a for loop to run through all_stocks ticker, then in each iteration, print out the data
+# for ticker in test_stocks:
+#     print("S2: ticker: ===================================", ticker)
+#     print(client.get_data_full(symbol=ticker))
+
+
+
+# S3: Convert json data into into PANDAS dataframe for easier manipulation
+pd_stocks = json.loads(client.resp._content.decode('utf-8'))['data'] # filter and extract for the companies that you have data
+pd_stocks = pd.DataFrame(pd_stocks) # transform it to a pandas dataframe
+print("S3: test_stocks Pandas DataFrame", pd_stocks) # Output the dataframe to VISUALIZE the data
+
+# S4: create stock objects for the test stocks
+"""
+# using numpy, get the average EPS growth for each stock, and create the stock object for each stock
+# get the most recent eps growth based on teh last eps diluted growth for each stock for inputting into the stock object later\
+# TODO DONE: (update stock name and current price) (currently set name to TEST, and set current price to -1)
+"""
+
+all_stocks = [] # initalize a list of stock objects
+
+for i in range(len(test_stocks)):
+    resp2 = client.get_data_full(symbol=test_stocks[i]) #TODO this takes decent amount of time
+    stock_name = resp2.get('metadata', {}).get('name', "Unknown")
+    ticker = test_stocks[i]
+    current_price = pd_stocks.loc[ticker, 'period_end_price'][-1]# alpha_vantage_current_price_obtainer(ticker)
+    total_current_assets = pd_stocks.loc[ticker, 'total_current_assets'][-1]
+    liabilities = pd_stocks.loc[ticker, 'total_current_liabilities'][-1]
+    market_cap = pd_stocks.loc[ticker, 'market_cap'][-1]
+    shares = pd_stocks.loc[ticker, 'shares_diluted'][-1]
+    stock = Stock(stock_name, ticker, total_current_assets, liabilities, market_cap, shares, current_price)
+    all_stocks.append(stock)
+
+print("S4: Stock Objects:") # Print the final stock objects based on the stringto method from all stocks
+for stock in all_stocks:
+    print(stock)
+
+
+# S5: See eps.py
+
+
+# ARCHIVES
+"""
+# Stock 1: AAPL
+# AAPL = client.get_data_full(symbol='AAPL:US') 
+# print(AAPL)
+
+# client.get_data_batch(companies=test_stocks, metrics=['eps', 'roic'], period="FY-2:FY") # Example Function
+
+# Old Current Price Retrieval
+# current_price = pd_stocks.loc[ticker, 'period_end_price'][-1] #TODO - uses period end prices (believe it is year end), not the current price
+
+"""
+
+
+
+
---
+++ graham_model/obj/Stock.py
@@ -0,0 +1,152 @@
+"""
+Stock object class for Ben graham's method
+
+TODO: write documentations for each method
+"""
+import numpy as np
+
+class Stock:
+    graham_factor = 0.33  # "Graham liked to buy stocks when net assets are 66% of market cap"
+
+    # Attributes
+    def __init__(self, stock_name, ticker, assets, liabilities, market_cap, shares,current_price):
+        self.stock_name = stock_name
+        self.ticker = ticker
+        #self.EPS_2023 = EPS_2023
+        #self.EPS_growth = EPS_growth # Array of 10yr EPS
+        #self.avg_EPS = self.get_average_EPS_growth(EPS_growth) # internal method
+        #self.PE = PE
+        #self.avg_PE = self.get_average_PE() # internal method
+        self.current_price = current_price
+        self.assets = assets
+        self.liabilities = liabilities
+        self.net_assets = self.assets-self.liabilities
+        self.market_cap = market_cap
+        self.shares = shares
+        self.net_asset_value = self.net_assets/self.shares
+        self.current_price_above_nav = (self.current_price-self.net_asset_value)/self.net_asset_value
+        #self.dampener = self.determine_dampener() # internal method
+        #self.buy_price = self.target_prices(self.avg_PE) #internal method
+        #self.current_price_above_below_buy_price_percent = self.get_current_price_above_below_buy_price_percent() #internal method
+
+    # General Util
+    def __str__(self):
+        # please print the entire stock object
+        return f"Stock: {self.stock_name} ({self.ticker})\n" + \
+            f"Assets: {self.assets}\n" + \
+            f"Liabilities: {self.liabilities}\n" + \
+            f"Shares Outstanding: {self.shares}\n" + \
+            f"Net Assets: {self.net_assets}\n" + \
+            f"Current Price: {self.current_price}\n" + \
+            f"Net Asset Value: {self.net_asset_value}\n" + \
+            f"Above below: {self.current_price_above_nav}\n"
+
+            #f"EPS 2023: {self.EPS_2023}\n" + \
+            #f"EPS Growth: {self.EPS_growth}\n" + \
+            #f"PE: {self.PE}\n" + \
+            #f"Average PE: {self.avg_PE}\n" + \
+            #f"Dampener: {self.dampener}\n" + \
+
+        # return f"{self.stock_name} ({self.ticker}) - Current Price: {self.current_price}"
+
+    # EPS Module
+    @staticmethod
+    # take the average eps growth and if it is lower than negative max eps growth or higher than max eps growth, clip it to the max eps growt
+    def get_average_EPS_growth(EPS_growth):
+        EPS_growth = np.clip(EPS_growth, -Stock.max_EPS_growth, Stock.max_EPS_growth)
+        return np.mean(EPS_growth)
+    
+    # take the percent above or below the buy price that the current price is at
+    def get_current_price_above_below_buy_price_percent(self):
+        
+        return (self.current_price-self.buy_price)/self.buy_price
+
+    # from the eps_growth array, sum up all the negative eps growth (below zero)
+    def determine_num_of_negative_growths(self):
+        return len([growth for growth in self.EPS_growth if growth <= 0]) #Edited - stocks without 10 yrs data are showing up because they have 0s for years they did not exist.
+        # return sum(1 for growth in self.EPS_growth if growth < 0)
+    
+    # Buy Sell Module
+    def get_average_PE(self):
+        return np.mean(self.PE)
+
+
+    # Buy Sell Util
+    ###
+    def predict_interest_rate(self, PE=None):
+        EPS_avg = self.get_average_EPS_growth(self.EPS_growth)
+        adjusted_EPS = EPS_avg * self.dampener
+        if PE is None:
+            PE = self.avg_PE
+        EPS_2033 = self.exponential_growth(self.EPS_2023, 1 + adjusted_EPS, 10)
+        price_2033 = PE * EPS_2033
+        growth_rate = (price_2033 / self.current_price) ** (0.1)
+        return growth_rate
+
+    @staticmethod
+    def exponential_growth(initial_value, growth_rate, num_iterations):
+        current_value = initial_value
+        for _ in range(num_iterations):
+            current_value *= growth_rate  # Apply growth rate
+        return current_value
+    ###
+
+    def determine_dampener(self):
+        dampener = self.max_dampener
+        for growth in self.EPS_growth:
+            if growth < 0:
+                dampener -= 0.1
+        if dampener != self.max_dampener:
+            dampener += 0.1
+        return dampener
+
+    def target_prices(self, target_PE):
+        EPS_avg = self.get_average_EPS_growth(self.EPS_growth)
+        adjusted_EPS = EPS_avg * self.dampener
+        EPS_2033 = self.exponential_growth(self.EPS_2023, 1 + adjusted_EPS, 10)
+        price_2033 = target_PE * EPS_2033
+        target_price = price_2033 / (self.target_rate + 1) ** 10
+        buy_price = target_price * (1 - self.margin_of_safety)
+        sell_price = price_2033 / (self.sell_rate + 1) ** 10
+        
+        #print(f"Using PE of {target_PE}, the original buy (target rate: {self.target_rate}) is {target_price}.")
+        #print(f"After margin of safety of {self.margin_of_safety}, the predicted buy is {buy_price}.")
+        #print(f"The predicted sell (sell rate: {self.sell_rate}) is {sell_price}.")
+        return buy_price #TODO this only returns the buy price, it should return the sell price and ideally the target price too - or can be made different functions
+   
+    # TESTING MODULE
+    def evaluate_old(self):
+        print(self)
+        s = f"\nAt the current price of {self.current_price}, the model's projected interest rate is "
+        s += str(self.predict_interest_rate())
+        s += f"\nUsing the newest PE, {self.PE[-1]}, the model's predicted interest rate is "
+        s += str(self.predict_interest_rate(self.PE[-1])) + f"\nDampener: {self.dampener}\n"
+        s += f"Years of negative EPS Growth: {self.determine_num_of_negative_growths()}\n"
+        print(s)
+
+        while True:
+            PE = float(input("Enter PE to evaluate, -997 for average, -998 for FYMost recent, -999 to exit: "))
+            if PE == -997:
+                print(f"Using Average PE of {self.get_average_PE()} and most recent EPS of {self.EPS_2023}, the model's predicted interest rate is ")
+                print(self.predict_interest_rate(self.get_average_PE()))
+            elif PE == -998:
+                print(f"Using the most recent PE of {self.PE[-1]} and most recent EPS of {self.EPS_2023}, the model's predicted interest rate is ")
+                print(self.predict_interest_rate(self.PE[-1]))
+            elif PE == -999:
+                break
+            else:
+                print(f"Using PE of {PE} and most recent EPS of {self.EPS_2023}, the model's predicted interest rate is ")
+                print(self.predict_interest_rate(PE))
+                self.target_prices(PE)
+
+    def evaluate(self):
+        print(self)
+        s = f"\nAt the current price of {self.current_price}, the model's projected interest rate is (using average PE) "
+        s += str(self.predict_interest_rate())
+        s += f"\nUsing the newest PE, {self.PE[-1]}, the model's predicted interest rate is "
+        s += str(self.predict_interest_rate(self.PE[-1]))# + f"\nDampener: {self.dampener}\n"
+        #s += f"Years of negative EPS Growth: {self.determine_num_of_negative_growths()}\n"
+        print(s)
+
+
+
